936febf8978fbb4c103675cc83b83ff71ac68b1c
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2024-07-20 07:41:41 -0600

    Base Project

---
+++ BreakoutV2/Assets/ViewportToWorlTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    void OnDrawGizmosSelected()
+    {
+        Camera camera = GetComponent<Camera>();
+        Vector3 p = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(p, 0.1F);
+    }
+}
